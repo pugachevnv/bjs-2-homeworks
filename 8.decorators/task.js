5bffacc0df509b587c8a0fb04b007beabfa38baa
pugachev.nv <pugachev.nv@gmail.com>
2023-10-21 11:29:00 +0300

    Решена 2 задача

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,38 @@
 //Задача № 1
+const md5 = require('./js-md5.js');
+
 function cachingDecoratorNew(func) {
-  
+    let cache = [];
+  const maxCacheValuesCount = 5;
+  return (...args) => {
+    
+  };
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+    let timeoutId = null;
+
+    function wrapper(...args){
+        clearTimeout(timeoutId);
+        wrapper.allCount++;
+
+        if(!timeoutId){
+            func(...args);
+            wrapper.count++;            
+        }
+
+        timeoutId = setTimeout(() => {
+            func(...args);
+            wrapper.count++;
+        }, delay);
+    }
+
+    wrapper.count = 0;
+    wrapper.allCount = 0;
+    
+    return wrapper;
 }
+
+
+ 
